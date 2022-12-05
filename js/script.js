a4a1aeb5aaf79c37f99fec572dcaa0c36268631d
Kevin Kuch <kuchkevin96@gmail.com>
2022-12-05 09:26:14 -0500

    Avancement TP4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ $(document).ready(function(){
     });
   });
 
+  // $(window).on('scroll', function() {
+  //   let parallax = $(".img-person-col");
+  //   let scrollPosition = $(this).scrollTop();
+  //   parallax.css("transform", "translateY(" + scrollPosition * 0.5 + "px" + ")");
+  // });
+
+// Effet de parralax
+
+// let imgCiel = document.getElementsByClassName('img-ciel');
+// let imgPerso = document.getElementsByClassName('img-person-sol');
+// let textAccueil = document.getElementsByClassName('text-accueil');
+
+// window.addEventListener('scroll', function() {
+//     let value = window.scrollY;
+
+//     imgCiel.style.top = value * 0.7 + 'px';
+//     imgPerso.style.bottom = value * 0.5 + 'px';
+//     textAccueil.style.top = value * 1 + 'px';
+// });
+
 
 
 
