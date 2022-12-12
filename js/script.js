aec1897beda45f2444b8593aa1ef2381397bad58
Kevin Kuch <kuchkevin96@gmail.com>
2022-12-12 01:21:47 -0500

    Ajout animation des contenus

--- js/script.js
+++ js/script.js
@@ -15,15 +15,30 @@ window.addEventListener('scroll', () => {
   // console.log(value);
   textAccueil.style.marginTop = value * 1.5 + 'px';
 
-  if (value > 600) {
+  if (value > 480) {
     textAccueil.style.display = "none";
   } else {
     textAccueil.style.display = "block";
   }
-
-
 });
 
 
+/******** Effect animation des contenus *******************/
+
+ScrollReveal({
+  reset: true,
+  duration: 1000,
+  distance: '20px',
+  easing: 'ease',
+  delay: 400
+})
+
+ScrollReveal().reveal('.text-accueil-anim', { delay: 600, origin: 'left', distance: '40px', interval: 100 });
+ScrollReveal().reveal('.planet-terre-accueil', { delay: 600, origin: 'bottom', distance: '50px' });
+ScrollReveal().reveal('.titre-section', { delay: 600, origin: 'left', distance: '40px' });
+ScrollReveal().reveal('.text-desc', { delay: 700, origin: 'bottom', interval: 150 });
+ScrollReveal().reveal('.btn-explore', { delay: 850, origin: 'right' });
+ScrollReveal().reveal('.experience-image', { delay: 650, origin: 'top', interval: 150 });
+ScrollReveal().reveal('.experience-image', { delay: 650, origin: 'top', interval: 150 });
 
 
